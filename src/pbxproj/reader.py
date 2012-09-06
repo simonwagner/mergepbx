13b3202e23de0c0eacaf795c983fa4c122b99ab6
Simon Wagner <wagner.sim1988@googlemail.com>
2012-09-06 12:34:46 +0200

    add ignore_unknown_objects parameter to read_pbx

--- src/pbxproj/reader.py
+++ src/pbxproj/reader.py
@@ -1,11 +1,11 @@
 from plist import NSPlistReader
 from .pbxobjects import PBXProjFile
 
-def read_pbx(pbx_file):
+def read_pbx(pbx_file, ignore_unknown_objects=False):
     f = open(pbx_file)
     r = NSPlistReader(f)
     plist = r.read()
-    project = PBXProjFile(plist)
+    project = PBXProjFile(plist, ignore_unknown_objects=False)
     r.close()
 
     return project
