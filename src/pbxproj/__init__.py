96bd790f7925689dfefb7798665f9a380e54449b
Simon Wagner <wagner.sim1988@googlemail.com>
2014-01-15 18:19:35 +0100

    add write method to pbxproj module

--- src/pbxproj/__init__.py
+++ src/pbxproj/__init__.py
@@ -10,3 +10,10 @@ def read(fname):
     f = open(fname)
     reader = NSPlistReader(f)
     return PBXProjFile(reader.read())
+
+def write(fname_or_f, data, encoding="utf-8"):
+    if isinstance(fname_or_f, str) or isinstance(fname_or_f, unicode):
+        f = open(fname, "w", encoding=encoding)
+    else:
+        f = fname_or_f
+    writer.write_pbx(data, f)
