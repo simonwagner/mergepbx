266ddccb6ec28db398cd13e8389737e31c5d7eeb
Simon Wagner <wagner.sim1988@googlemail.com>
2014-01-16 23:08:06 +0100

    Make test_pbxmerge more beautiful.

--- test/test_pbxmerge.py
+++ test/test_pbxmerge.py
@@ -13,12 +13,9 @@ class PBXMergeTest(unittest.TestCase):
         expected_mergers = set(isa_name + "Merger3" for isa_name in isa_names) - set(("PBXISAMerger3",))
         available_merger_names = set(pbxmerge.MERGER_MAPPING.keys())
 
-        missing_mergers = expected_mergers - available_merger_names
+        for expected_merger in expected_mergers:
+            self.assertIn(expected_merger, available_merger_names, "Missing merger %s" % expected_merger)
 
-        for merger in sorted(missing_mergers):
-            print "Missing Merger: %s" % merger
-
-        self.assertEqual(0, len(missing_mergers))
 
 if __name__ == '__main__':
     unittest.main()
