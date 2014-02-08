78196874efe2f8bd790daa3761a00069ed6eb112
Simon Wagner <wagner.sim1988@googlemail.com>
2014-02-08 13:04:07 +0100

    Run tests under debugger when --debug is given on the command line.

--- tools/test.py
+++ tools/test.py
@@ -26,6 +26,9 @@ def get_argument_parser():
     parser.add_argument("--test-logging",
                         help="show only log messages from test cases",
                         action="store_true")
+    parser.add_argument("--debug",
+                        help="start the debugger when an exception is thrown",
+                        action="store_true")
 
     return parser
 
@@ -42,6 +45,22 @@ def setup_logging():
 
     return ch
 
+def install_pdb_exception_handler():
+    def info(type, value, tb):
+       if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+          # we are in interactive mode or we don't have a tty-like
+          # device, so we call the default hook
+          sys.__excepthook__(type, value, tb)
+       else:
+          import traceback, pdb
+          # we are NOT in interactive mode, print the exception...
+          traceback.print_exception(type, value, tb)
+          print
+          # ...then start the debugger in post-mortem mode.
+          pdb.pm()
+
+    sys.excepthook = info
+
 if __name__ == '__main__':
     log_handler = None
 
@@ -52,8 +71,16 @@ if __name__ == '__main__':
     if args.test_logging and log_handler == None:
         handler = setup_logging()
         handler.addFilter(OnlyTestLogsFilter())
+    if args.debug:
+        install_pdb_exception_handler()
 
     loader = unittest.TestLoader()
     tests = loader.discover(TEST_DIR)
-    testRunner = unittest.runner.TextTestRunner()
-    testRunner.run(tests)
+
+    if args.debug:
+        print "Running tests in debug mode..."
+        for test in tests:
+            test.debug()
+    else:
+        testRunner = unittest.runner.TextTestRunner()
+        testRunner.run(tests)
