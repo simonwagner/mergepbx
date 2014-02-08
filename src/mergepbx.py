665d036ebcc5a2f0ca6c3194646eca7a9fe39d2d
Simon Wagner <wagner.sim1988@googlemail.com>
2014-02-08 15:59:15 +0100

    Add --debug cmd option to mergepbx
    If --debug is given on the command line, the debugger will be started when an exception occurs.

--- src/mergepbx.py
+++ src/mergepbx.py
@@ -21,8 +21,27 @@ def get_argument_parser():
     parser.add_argument("-o", "--output",
                         help="output path for the merged file",
                         default=None)
+    parser.add_argument("-d", "--debug",
+                        help="enable debugger on exceptions",
+                        action="store_true")
 
     return parser
+
+def install_pdb_exception_handler():
+    def info(type, value, tb):
+       if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+          # we are in interactive mode or we don't have a tty-like
+          # device, so we call the default hook
+          sys.__excepthook__(type, value, tb)
+       else:
+          import traceback, pdb
+          # we are NOT in interactive mode, print the exception...
+          traceback.print_exception(type, value, tb)
+          print
+          # ...then start the debugger in post-mortem mode.
+          pdb.pm()
+
+    sys.excepthook = info
 
 def main():
     log = sys.stderr
@@ -34,12 +53,19 @@ def main():
     else:
         output = args.mine
 
-    try:
+    if args.debug:
+        #if debugging is enabled, install the pdb
+        #handler and let him handle the exception
+        install_pdb_exception_handler()
         merge_pbx_files(args.base, args.mine, args.theirs, output)
-        sys.exit(0)
-    except Exception as e:
-        log.write("merging failed: %s\n" % str(e))
-        sys.exit(1)
+    else:
+        #if debugging is not enabled, simply report the exception
+        try:
+            merge_pbx_files(args.base, args.mine, args.theirs, output)
+            sys.exit(0)
+        except Exception as e:
+            log.write("merging failed: %s\n" % str(e))
+            sys.exit(1)
 
 def merge_pbx_files(basef, minef, theirsf, mergedf):
     base, mine, theirs = read_pbxs((basef, minef, theirsf))
